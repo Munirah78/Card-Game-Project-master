f2cc19328a60631b8221eeb800f72148b851a932
munirah78 <imema.21@gmail.com>
2021-11-24 03:07:10 +0300

    first commit

---
+++ Card-Game-Project-master/js/app.js
@@ -0,0 +1,79 @@
+
+function shuffle(array) {
+var currentIndex = array.length, temporaryValue, randomIndex;
+
+while (0 !== currentIndex) {
+randomIndex = Math.floor(Math.random() * currentIndex);
+currentIndex -= 1;
+temporaryValue = array[currentIndex];
+array[currentIndex] = array[randomIndex];
+array[randomIndex] = temporaryValue;
+}
+
+return array;
+}
+const cards = document.querySelectorAll('.card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+  
+    this.classList.add('flip');
+  
+    if (!hasFlippedCard) {
+      // first click
+      hasFlippedCard = true;
+      firstCard = this;
+  
+      return;
+    }
+  
+    // second click
+    secondCard = this;
+  
+    checkForMatch();
+  }
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  
+    isMatch ? disableCards() : unflipCards();
+  }
+  
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+  
+    resetBoard();
+  }
+  
+  function unflipCards() {
+    lockBoard = true;
+  
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+  
+      resetBoard();
+    }, 1500);
+  }
+  
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+
+
+
+
+
+
+
+
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
+
